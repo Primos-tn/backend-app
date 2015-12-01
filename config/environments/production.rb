bcec59252cec324d2543e9988f8b876a66ec8c6d
Houssem Fathallah <houssem.fathallah@proxym-it.tn>
2015-12-01 14:32:38 +0100

    add assets file generation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   
-  
-  config.serve_static_assets = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
