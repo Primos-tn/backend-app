99794c058c19b3bd7db9aef0ae9d41443bbf8cae
hassenfath <hassenfathallah@gmail.com>
2015-12-06 03:10:03 -0500

    add serve

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  
+
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -23,7 +23,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = true 
+  config.serve_static_files = true
+  config.serve_static_assets = true
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
