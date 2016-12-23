9660fbb1dc8f043d748427da162e6a0dffbd3e56
hassenfath <fathallah.houssem@gmail.com>
2016-12-23 00:38:16 -0500

    fix errors on sending mail for business request

--- app/mailers/business_mailer.rb
+++ app/mailers/business_mailer.rb
@@ -2,16 +2,19 @@ class BusinessMailer < ApplicationMailer
   default :template_path => 'mailers/business'
 
   def new_business_request(current_user, business_profile)
+    @business_profile = business_profile
     mail(to: business_profile.business_email, subject: I18n.t('Update'))
   end
 
 
   def confirm_business_request(account, business_profile)
+    @business_profile = business_profile
     mail(to: business_profile.business_email, subject: I18n.t('Update'))
   end
 
 
   def free_trial_business(account, business_profile)
+    @business_profile = business_profile
     mail(to: business_profile.business_email, subject: I18n.t('Welcome'))
   end
 
@@ -26,4 +29,6 @@ class BusinessMailer < ApplicationMailer
     @reason = ""
     mail(to: business_profile.business_email, subject: I18n.t('Update'))
   end
+
+
 end
